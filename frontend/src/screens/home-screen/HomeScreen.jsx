99ebd86b71981c9ce1cae4b74161d7531c0e1dbb
POkulska <paulina.okulska@gmail.com>
2022-09-20 20:33:34 +0200

    added helmet to home and product screen

--- frontend/src/screens/home-screen/HomeScreen.jsx
+++ frontend/src/screens/home-screen/HomeScreen.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import useApi from "../../utils/customHooks.js";
-import {  CircularProgress, Grid } from "@mui/material";
+import { CircularProgress, Grid } from "@mui/material";
 import CardComponent from "../../components/card/CardComponent.jsx";
 import ResponsiveGrid from "../../components/responsie-grid/ResponsiveGrid.js";
-import {MdError} from 'react-icons/md';
+import { MdError } from "react-icons/md";
+import { Helmet } from "react-helmet-async";
 
 function HomeScreen() {
   const { loading, error, data } = useApi("/api/products");
 
   return (
     <div>
+      <Helmet>
+        <title>Kebab shop</title>
+      </Helmet>
       <h2>Our dishes</h2>
       <h1>Popular dishes</h1>
 
       <div className="products">
-        {loading ? <CircularProgress />
-         : 
-
-         error ? <MdError/> 
-         :
-        (
+        {loading ? (
+          <CircularProgress />
+        ) : error ? (
+          <MdError />
+        ) : (
           <ResponsiveGrid>
             {data.map((product) => {
               if (product.category === "food") {
                 return (
-                  <Grid item xs={12} sm={6} md={2}  key={product.slug}>
+                  <Grid item xs={12} sm={6} md={2} key={product.slug}>
                     <CardComponent product={product} />
                   </Grid>
                 );
@@ -35,15 +38,13 @@ function HomeScreen() {
       </div>
     </div>
   );
-};
+}
 
-
-export default HomeScreen
+export default HomeScreen;
 // export default HomeScreen
 
-
 //{
-  /* <div className="product" key={product.slug}>
+/* <div className="product" key={product.slug}>
                     <Link to={`/product/${product.slug}`}>
                       <img src={product.image} alt={product.name} />
                     </Link>
--- frontend/src/screens/product-screen/ProductScreen.jsx
+++ frontend/src/screens/product-screen/ProductScreen.jsx
@@ -1,25 +1,105 @@
-import React from 'react';
-import  {useParams} from 'react-router-dom';
-import useApi from '../../utils/customHooks';
-import {  CircularProgress } from "@mui/material";
-import {MdError} from 'react-icons/md';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useApi from "../../utils/customHooks";
+import {
+  Button,
+  CircularProgress,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { MdError } from "react-icons/md";
+import { Box, Container } from "@mui/system";
+import RatingComponent from "../../components/rating/Rating";
+import KebabDiningIcon from "@mui/icons-material/KebabDining";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import DescriptionIcon from "@mui/icons-material/Description";
+import {Helmet} from 'react-helmet-async';
 
 export default function ProductScreen() {
-  const {slug} = useParams();
-  const { loading, error, data } = useApi(`/api/products/slug/${slug}`,slug);
+  const { slug } = useParams();
+  const { loading, error, data } = useApi(`/api/products/slug/${slug}`, slug);
 
-   
+  return loading ? (
+    <CircularProgress />
+  ) : error ? (
+    <MdError />
+  ) : (
+    <Container>
+      <Box
+        display="flex"
+        flexDirection={{ xs: "column", md: "row" }}
+        alignContent={"center"}
+      >
+        <Box>
+          <figure className="product-image">
+            <img src={data.image} alt={data.name} />{" "}
+          </figure>
+        </Box>
 
-  return    (
-    
-      loading ? <CircularProgress />
-  :   error ? <MdError/> 
-  : (<div>
-    <h1>{data.name}</h1>
-  </div>
-)
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <KebabDiningIcon />
+              </ListItemIcon>
+              <Helmet>
+                <title>{data.name}</title>
+              </Helmet>
+              <ListItemText primary={data.name} />
+            </ListItemButton>
+          </ListItem>
 
-    
-    
-  )
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <AttachMoneyIcon />
+              </ListItemIcon>
+              <ListItemText primary={`Price:${data.currency} ${data.price}`} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <RatingComponent
+                rating={data.rating}
+                numReviews={data.numReviews}
+              />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DescriptionIcon />
+              </ListItemIcon>
+              <ListItemText primary={data.description} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              {data.countInStock > 0 ? (
+                <Button variant="contained" color="success">
+                  Order now
+                </Button>
+              ) : (
+                <Button variant="contained" color="error">
+                  Unavailable
+                </Button>
+              )}
+              {/* <ListItemText primary={`Status: ${data.countInStock>0 ? data.countInStock : "unavailable"}`} /> */}
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </Container>
+  );
+}
+
+{
+  /* <ListItemText vriant="text" color="success" primary={`Status:`} /> */
 }
